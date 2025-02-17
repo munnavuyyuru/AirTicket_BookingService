705e2ebf00e280a6759eb853f10f9b8ea826744b
munnavuyyuru <111678429+munnavuyyuru@users.noreply.github.com>
2025-02-17 20:23:45 +0530

    added rabbitmq

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,27 +1,42 @@
 const { StatusCodes } = require("http-status-codes");
+
+const { createChannel, publishMessage } = require("../utils/messageQueue");
+const { REMINDER_BINDING_KEY } = require("../config/serverConfig");
 const BookingService = require("../service/booking-service");
 
 const bookingService = new BookingService();
 
-const create = async (req, res) => {
-  try {
-    const response = await bookingService.createBooking(req.body);
-    console.log("Inside the booking controller : ", response);
-    return res.status(StatusCodes.OK).json({
-      data: response,
-      success: true,
-      message: "successfully booked a flight",
-      error: {},
-    });
-  } catch (error) {
-    return res.status(error.StatusCode).json({
-      data: {},
-      success: false,
-      message: error.message,
-      error: error.explination,
+class BookingController {
+  constructor() {}
+
+  async sendMessageToQueue(req, res) {
+    const channel = await createChannel();
+    const data = { message: "SUCCESS" };
+    publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+    return res.status(200).json({
+      message: "Succesfully published the event",
     });
   }
-};
-module.exports = {
-  create,
-};
+
+  async create(req, res) {
+    try {
+      const response = await bookingService.createBooking(req.body);
+      console.log("Inside the booking controller : ", response);
+      return res.status(StatusCodes.OK).json({
+        data: response,
+        success: true,
+        message: "successfully booked a flight",
+        error: {},
+      });
+    } catch (error) {
+      return res.status(error.StatusCode).json({
+        data: {},
+        success: false,
+        message: error.message,
+        error: error.explination,
+      });
+    }
+  }
+}
+
+module.exports = BookingController;
